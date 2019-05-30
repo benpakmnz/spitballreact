05908fdba26015d71b845a7598c310ec8c49745f
benny <benpakmnz@gmail.com>
2019-05-30 09:03:41 +0300

    Updates

---
+++ precache-manifest.299891f1c01202347a4460bc5a7897ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ef5eb6d2d81b9c82f00e61cfafb6c54",
+    "url": "/spitballreact/index.html"
+  },
+  {
+    "revision": "b42b147013a92df3f923",
+    "url": "/spitballreact/static/css/main.b0ed6f3a.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/spitballreact/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "b42b147013a92df3f923",
+    "url": "/spitballreact/static/js/main.8397ada7.chunk.js"
+  },
+  {
+    "revision": "9533bc54ba4ec20cee6c",
+    "url": "/spitballreact/static/js/runtime~main.757c8b75.js"
+  }
+]);
